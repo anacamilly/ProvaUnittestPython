bb58e7004f6027d830cb6d668819fec6e8c76b8c
Ana Camilly <camillyguedes2@gmail.com>
2022-07-27 20:06:22 -0300

    correção de erros

--- questao4.py
+++ questao4.py
@@ -1,8 +1,12 @@
 def alunosMatriculados(id_turma, dados):
-	resultado = []
+	resultado1 = 0
+	resultado2 = 0
 	for i in range(len(dados)):
 		if id_turma == dados[i][0]:
-			resultado = True
+			resultado1 = resultado1 + 1
 		else:
-			resultado = False
-	return (resultado)
+			resultado2 = resultado2 + 1
+	if resultado1 > 0 and resultado2 == 0:
+		return (True)
+	else:
+		return (False)
--- test/test_questao4.py
+++ test/test_questao4.py
@@ -9,3 +9,4 @@ class TodosAlunosMatric(unittest.TestCase):
 	def testAlunosMatriculados(self):
 		list = [(1, 1), (1 ,2), (1, 3), (2, 1), (2, 4)]
 		self.assertEqual(alunosMatriculados(1, list), (False))
+		self.assertEqual(alunosMatriculados(2, list), (False))
